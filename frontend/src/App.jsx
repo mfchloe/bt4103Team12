7a82b0326db1ac87b0b1cba960514886e33f1930
myathetchai <myathetchai441@gmail.com>
2025-10-06 22:47:56 +0800

    feat(not-found-page): done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Layout from "./components/Layout";
 // actual pages
 import Home from "./pages/Home";
 import TimeSeries from "./pages/TimeSeries";
+import FARDashboard from "./pages/FARDashboard";
 import NotFound from "./pages/NotFound";
 
 const App = () => {
@@ -14,6 +15,9 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/timeseries" element={<TimeSeries />} />
+          <Route path="/far-dashboard" element={<FARDashboard />} />
+          {/* catch-all for not found page!! */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
     </BrowserRouter>
---
+++ frontend/src/pages/FARDashboard.jsx
@@ -0,0 +1,12 @@
+import { Typography, Box } from "@mui/material";
+import React from "react";
+
+const FARDashboard = () => {
+  return (
+    <Box>
+      <Typography>FAR Dashboard page, coming SOON</Typography>
+    </Box>
+  );
+};
+
+export default FARDashboard;
